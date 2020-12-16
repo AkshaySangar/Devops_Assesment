3b7eb5a436c6f0ab5fb627a6a97ee84615edba13
AkshaySangar <akshaysangar9@gmail.com>
2020-12-16 12:38:33 +0530

    Source folder of python file

---
+++ Sources/main.py
@@ -0,0 +1,81 @@
+import pandas as pd
+
+
+class Account:
+
+    account_number = []
+    name = []
+    mobile_no = []
+    balance = []
+    #account_type = []
+    data = {'Customer_Name': name, '  Account': account_number,  '  Balance': balance, '  Mobile_No.': mobile_no}
+
+    df = pd.DataFrame(data)
+
+    def set_data(self):
+
+        self.df = pd.DataFrame(self.data)
+
+    def create_account(self, name1,acc_number,mbl_no,bal):
+
+        #print('Enter name : ')
+        self.name.append(name1)
+        #print('Enter Account Number')
+        self.account_number.append(acc_number)
+        #print('Enter Mobile Number')
+        self.mobile_no.append(mbl_no)
+        #print('Enter amount credit')
+        self.balance.append(bal)
+        #print('Account is created')
+        self.set_data()
+
+    def display_all_records(self):
+        self.set_data()
+        #print(self.df)
+        return self.df
+
+    def display_by_account_number(self,acc_number):
+        self.set_data()
+        #print('Enter account number')
+        #acc_number = input()
+        for i in range(0, len(self.account_number)):
+            if acc_number == self.account_number[i]:
+                acc=self.df.loc[[i]]
+                return acc
+                break
+
+    def credit(self,acc_number,amount):
+
+        print('Enter account number')
+        # acc_number = input()
+        print('Enter amount to credit')
+        # amount = input()
+        for i in range(0, len(self.account_number)):
+            if acc_number == self.account_number[i]:
+                self.balance[i] = int(self.balance[i])+int(amount)
+                return self.balance[i]
+                #print('Amount credited')
+                self.set_data()
+                break
+
+    def debit(self,acc_number,amount):
+
+        print('Enter account number')
+        # acc_number = input()
+        print('Enter amount to credit')
+        # amount = input()
+        for i in range(0, len(self.account_number)):
+            if acc_number == self.account_number[i]:
+                self.balance[i] = self.balance[i] - amount
+                return self.balance[i]
+                #print('Account Debited')
+                self.set_data()
+                break
+
+
+if __name__ == "__main__":
+    s = Account()
+    s.create_account('Akshay',1,8421966287,1000)
+    #df1=s.display_all_records()
+    s.display_by_account_number(1)
+    #print(df1)
---
+++ Sources/test.py
@@ -0,0 +1,70 @@
+import unittest
+import main as m
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
+
+
+class Test(unittest.TestCase):
+    account_number = []
+    name = []
+    mobile_no = []
+    balance = []
+    # account_type = []
+    data = {'Customer_Name': name, '  Account': account_number, '  Balance': balance, '  Mobile_No.': mobile_no}
+    # df = pd.DataFrame(data)
+
+    s = m.Account()
+    s.create_account('Akshay', 1, 8421966287, 1000)
+    name.append('Akshay')
+    account_number.append(1)
+    mobile_no.append(8421966287)
+    balance.append(1000)
+    df = pd.DataFrame(data)
+
+    def test_all_record(self):
+        s = m.Account()
+        df1 = s.display_all_records()
+        assert_frame_equal(df1, self.df)
+
+    def test_record_by_acc_no(self):
+        s = m.Account()
+        acc_number = 1
+        df1 = s.display_by_account_number(1)
+        for i in range(0, len(self.account_number)):
+            if acc_number == self.account_number[i]:
+                acc = self.df.loc[[i]]
+                assert_frame_equal(df1, acc)
+                break
+
+    def test_credit(self):
+
+        s = m.Account()
+        bal = s.credit(1,1000)
+        acc_number = 1
+        amount = 1000
+        for i in range(0, len(self.account_number)):
+            if acc_number == self.account_number[i]:
+                self.balance[i] = int(self.balance[i])+int(amount)
+                self.df = pd.DataFrame(self.data)
+                break
+
+
+        self.assertEqual(bal,2000)
+
+    def test_debit(self):
+
+        s = m.Account()
+        bal = s.debit(1,1000)
+        acc_number = 1
+        amount = 1000
+        for i in range(0, len(self.account_number)):
+            if acc_number == self.account_number[i]:
+                self.balance[i] = self.balance[i] - amount
+                self.df = pd.DataFrame(self.data)
+                break
+
+        self.assertEqual(bal,1000)
+
+
+if __name__ == '__main__':
+    unittest.main()
